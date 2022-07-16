12206f6b5a2cb78abc959ce1ec93a8dad3b16c18
MarcioGomes78 <mjgomes1978@gmail.com>
2022-07-16 16:35:39 -0300

    Toast: notificação customizada

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/header"
 import SaleCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
-
       <main>
         <section id="sales">
           <div className="dsmeta-container">
