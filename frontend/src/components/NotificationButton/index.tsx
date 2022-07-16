2d937202af3f7d036a0515b56358b56b22ad605c
MarcioGomes78 <mjgomes1978@gmail.com>
2022-07-15 23:37:42 -0300

    Axios, useEffect first request

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -4,7 +4,7 @@ import "./style.css"
 function NotificationButton() {
   return (
     <div className="dsmeta-red-btn">
-      <img src={icon} alt="Notificar" />
+      <img src={icon} alt="Notificar"/>
     </div>
   )
 }
